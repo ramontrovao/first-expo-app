58bc0966548528151d1b4f1de1d989dfa8d49298
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-03-11 12:34:42 -0300

    feat: added form to add participants (#03)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
 
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  const handleAddParticipant = () => {
+    Alert.alert(
+      "Você clicou!",
+      "Tô em todo lugar, não dá pra controllah, OHOHO HAHA OHOHO HAHA"
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Festa do Pombo</Text>
+      <Text style={styles.eventName}>Festa da Nayeon</Text>
 
       <Text style={styles.eventDate}>Sábado, 11 de Março de 2023</Text>
+
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do convidado"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,9 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#131016",
     padding: 24,
+
+    flex: 1,
+
+    backgroundColor: "#131016",
   },
   eventName: {
     color: "#FDFCFE",
@@ -15,5 +17,38 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: "#6B6B6B",
     fontSize: 16,
+  },
+  formContainer: {
+    width: "100%",
+    marginTop: 36,
+    marginBottom: 42,
+
+    flexDirection: "row",
+    gap: 8,
+  },
+  input: {
+    flex: 1,
+
+    height: 56,
+    padding: 16,
+
+    backgroundColor: "#1F1E25",
+    color: "#FFF",
+    borderRadius: 5,
+    fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+
+    justifyContent: "center",
+    alignItems: "center",
+
+    backgroundColor: "#31CF67",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
   },
 });
